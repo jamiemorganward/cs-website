b80485441f774e7bf1ca133b77b1c668c64185f3
Jamie Morgan-Ward <jamie@formfunfunction.com>
2024-02-09 20:56:57 +1300

    initial commit

--- src/components/three-scene/ThreeScene.tsx
+++ src/components/three-scene/ThreeScene.tsx
@@ -13,10 +13,10 @@ export const ThreeScene: React.FC<ThreeSceneProps> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!containerRef.current) {
-      return
-    }
-    if (typeof window !== 'undefined') {
+    const load = async () => {
+      if (!containerRef.current) {
+        return
+      }
       if (typeof window !== 'undefined') {
         const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(
@@ -25,30 +25,53 @@ export const ThreeScene: React.FC<ThreeSceneProps> = ({ children }) => {
           0.1,
           1000
         )
-        const renderer = new THREE.WebGLRenderer()
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+
         renderer.setSize(window.innerWidth, window.innerHeight)
         containerRef.current?.appendChild(renderer.domElement)
         camera.position.z = 5
         camera.rotation.order = 'YXZ'
 
-        const geometry = new THREE.BoxGeometry()
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-        // const cube = new THREE.Mesh(geometry, material)
-        const cubeFar = new THREE.Mesh(geometry, material)
-        const cubeFarLeft = new THREE.Mesh(geometry, material)
-        const cubeFarRight = new THREE.Mesh(geometry, material)
-        cubeFar.position.z = -10
-        cubeFarLeft.position.z = -9
-        cubeFarLeft.position.x = 5
-        cubeFarRight.position.z = -9
-        cubeFarRight.position.x = -5
-        const cubeNear = new THREE.Mesh(geometry, material)
-        cubeNear.position.z = 10
-        // scene.add(cube)
-        scene.add(cubeFar)
-        scene.add(cubeNear)
-        scene.add(cubeFarLeft)
-        scene.add(cubeFarRight)
+        // scene.background = new THREE.Color(0xcccccc)
+        scene.fog = new THREE.FogExp2(0xcccccc, 0.002)
+
+        const geometry = new THREE.BoxGeometry(8, 8)
+        // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        new THREE.ImageBitmapLoader().load('/louie.jpg', (data) => {
+          const texture = new THREE.CanvasTexture(data)
+          const material = new THREE.MeshBasicMaterial({
+            // color: 0xffffff,
+            // flatShading: true,
+            map: texture
+          })
+          // const cube = new THREE.Mesh(geometry, material)
+          const cubeFar = new THREE.Mesh(geometry, material)
+          const cubeFarLeft = new THREE.Mesh(geometry, material)
+          const cubeFarRight = new THREE.Mesh(geometry, material)
+          cubeFar.position.z = -10
+          cubeFarLeft.position.z = -9
+          cubeFarLeft.position.x = 5
+          cubeFarRight.position.z = -9
+          cubeFarRight.position.x = -5
+          const cubeNear = new THREE.Mesh(geometry, material)
+          cubeNear.position.z = 10
+          // scene.add(cube)
+          scene.add(cubeFar)
+          scene.add(cubeNear)
+          scene.add(cubeFarLeft)
+          scene.add(cubeFarRight)
+
+          const dirLight1 = new THREE.DirectionalLight(0xffffff, 3)
+          dirLight1.position.set(1, 1, 1)
+          scene.add(dirLight1)
+
+          const dirLight2 = new THREE.DirectionalLight(0xffffff, 1)
+          dirLight2.position.set(-1, -1, 1)
+          scene.add(dirLight2)
+
+          const ambientLight = new THREE.AmbientLight(0x555555)
+          scene.add(ambientLight)
+        })
 
         // Render the scene and camera
         renderer.render(scene, camera)
@@ -85,6 +108,7 @@ export const ThreeScene: React.FC<ThreeSceneProps> = ({ children }) => {
         }
       }
     }
+    load()
   }, [])
 
   return <div ref={containerRef} />
