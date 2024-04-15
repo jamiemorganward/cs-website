fa01dde0a59c890d3f1ce572c0bca3ede6cc1941
Jack Branthwaite <jack@formfunfunction.com>
2024-04-15 15:02:58 +1200

    Update to images in project page

--- src/components/flexible-content/single-image-block/SingleImageBlock.tsx
+++ src/components/flexible-content/single-image-block/SingleImageBlock.tsx
@@ -1,5 +1,6 @@
 import { SingleImageBlockRecord } from '@/graphql/generated/graphql'
 import s from './SingleImageBlock.module.scss'
+import MuxPlayer from '@mux/mux-player-react'
 
 export const SingleImageBlock = ({
   data
@@ -8,11 +9,16 @@ export const SingleImageBlock = ({
 }) => {
   return (
     <div className={`${s.wrapper} ${data.fullwidth ? s.fullwidth : ''}`}>
-      <img
-        src={`${data.image?.responsiveImage?.src}`}
-        alt={`${data.image?.responsiveImage?.alt}`}
-        title={`${data.image?.responsiveImage?.title}`}
-      />
+      {data.image?.responsiveImage && (
+        <img
+          src={`${data.image?.responsiveImage?.src}`}
+          alt={`${data.image?.responsiveImage?.alt}`}
+          title={`${data.image?.responsiveImage?.title}`}
+        />
+      )}
+      {data.image?.video && (
+        <MuxPlayer src={data.image.video.streamingUrl} autoPlay="any" loop />
+      )}
     </div>
   )
 }
