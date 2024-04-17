b93dd268391ca0c95022badc1e5eed5282a7c695
Meike Ahlers <meike@clicksuite.co.nz>
2024-04-17 18:24:18 +1200

    added mouse hover effect and active state to desktop nav

--- src/components/header/desktop-menu/menu-item/MenuItem.tsx
+++ src/components/header/desktop-menu/menu-item/MenuItem.tsx
@@ -3,7 +3,9 @@ import s from './MenuItem.module.scss'
 import { usePathname, useRouter } from 'next/navigation'
 import CloseIcon from '../../../../assets/svgs/close.svg'
 import { PageInfoContext } from '@/lib/contexts/PageInfoContext'
-import { useContext } from 'react'
+import { useContext, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 export const MenuItem = ({
   text,
@@ -24,15 +26,62 @@ export const MenuItem = ({
 
   const isActive = (link: string) => pathname.includes(link)
   const showPill = (link: string) => pathname.includes(`${link}/`)
+  const linkRef = useRef<HTMLAnchorElement | null>(null)
+  const lineRef = useRef<HTMLSpanElement | null>(null)
+
+  gsap.registerPlugin(useGSAP)
+
+  const tl = gsap.timeline({ paused: true, defaults: { duration: 0.5 } })
+  let exitTime = 0
+
+  useGSAP(() => {
+    reset()
+    tl.to(lineRef.current, { width: '100%' })
+    tl.addPause('exit')
+    exitTime = tl.duration()
+    tl.to(lineRef.current, { x: '100%' })
+  }, [{ lineRef, linkRef }])
+
+  const reset = () => {
+    gsap.set(lineRef.current, {
+      width: '0%',
+      transformOrigin: 'left center',
+      x: 0
+    })
+  }
+
+  const hoverIn = () => {
+    if (tl.time() < exitTime) {
+      reset()
+      tl.play()
+    } else {
+      tl.restart()
+    }
+  }
+
+  const hoverOut = () => {
+    if (tl.time() < exitTime) {
+      tl.reverse()
+    } else {
+      tl.play()
+    }
+  }
 
   return (
     <Link
+      onMouseEnter={() => !isActive(link) && hoverIn()}
+      onMouseLeave={() => !isActive(link) && hoverOut()}
       className={`${s.link} ${sticky ? s.sticky : ''} ${
         isActive(link) ? s.active : ''
       }`}
       href={link}
+      ref={linkRef}
     >
-      <span className={s.linkText}>{text}</span>
+      <div className={s.linkText}>
+        {text}
+        <span ref={lineRef} className={s.linkHoverLine}></span>
+        {isActive(link) && <span className={s.linkActiveLine}></span>}
+      </div>
       {hasSubMenuPill && showPill(link) && (
         <span className={s.submenuPill}>
           {subMenuText ? subMenuText : ctx.projectName ? ctx.projectName : ''}
