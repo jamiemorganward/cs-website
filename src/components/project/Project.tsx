01f6ef3324a27111075f5565c51e77e6216d9712
Jack <jack@clicksuite.co.nz>
2024-05-02 14:09:57 +1200

    Fix for background colour only working on video view

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -60,15 +60,17 @@ export const Project = ({
               <div className={s.projectName}>{year}</div>
             </div>
           )}
-          {image && image?.responsiveImage && (
-            <img
-              className={`${s.featuredImage}
+          <div className={s.imageWrapper} style={{ backgroundColor: colour }}>
+            {image && image?.responsiveImage && (
+              <img
+                className={`${s.featuredImage}
         ${alignment === 'left' && s.left}
         ${alignment === 'right' && s.right}
         ${(alignment === 'fullwidth' || fullwidth) && s.fullWidth}`}
-              src={image.responsiveImage.src}
-            />
-          )}
+                src={image.responsiveImage.src}
+              />
+            )}
+          </div>
           {video && (
             <div
               className={`${s.videoWrapper} ${fullwidth ? s.fullWidth : ''}`}
@@ -98,15 +100,17 @@ export const Project = ({
               <div className={s.projectName}>{year}</div>
             </div>
           )}
-          {image && image?.responsiveImage && (
-            <img
-              className={`${s.featuredImage}
+          <div className={s.imageWrapper} style={{ backgroundColor: colour }}>
+            {image && image?.responsiveImage && (
+              <img
+                className={`${s.featuredImage}
         ${alignment === 'left' && s.left}
         ${alignment === 'right' && s.right}
         ${alignment === 'fullwidth' && s.fullWidth}`}
-              src={image.responsiveImage.src}
-            />
-          )}
+                src={image.responsiveImage.src}
+              />
+            )}
+          </div>
           {video && (
             <div
               className={`${s.videoWrapper} ${fullwidth ? s.fullWidth : ''}`}
