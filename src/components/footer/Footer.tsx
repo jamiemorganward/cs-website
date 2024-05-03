9a02c275d60bcefa8acbbdd9ae742e589959daa2
Meike Ahlers <meike@clicksuite.co.nz>
2024-05-03 13:49:15 +1200

    removed older trial and error versions of heart animations added gradient

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,15 +2,7 @@
 import { DesktopMenu } from '../header/desktop-menu/DesktopMenu'
 import s from './Footer.module.scss'
 import LogoText from '../../assets/svgs/logo-text.svg'
-import { HeartAnimation } from '../heart-animation/HeartAnimation'
-import { HeartCanvas4 } from '../heart-animation/HeartCanvas4'
 import { HeartCanvas } from '../heart-animation/HeartCanvas'
-import { HeartCanvas2 } from '../heart-animation/HeartCanvas2'
-import { HeartCanvas3 } from '../heart-animation/HeartCanvas3'
-import { HeartThreeJs } from '../heart-animation/HeartThreeJs'
-import HeartCanvas5 from '../heart-animation/HeartCanvas5'
-import { HeartCanvas6 } from '../heart-animation/HeartCanvas6'
-import { HeartCanvas7 } from '../heart-animation/HeartCanvas7'
 
 export const Footer = () => {
   return (
@@ -24,7 +16,7 @@ export const Footer = () => {
         </div>
       </div>
       <div className={s.interactiveBit}>
-        <HeartCanvas4 />
+        <HeartCanvas />
       </div>
     </div>
   )
