9500cd0f3d76e67963eab7ff43fcfede09235bf3
Jack Branthwaite <jack@formfunfunction.com>
2024-08-12 11:51:16 +1200

    Meta

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -5,11 +5,34 @@ import {
   GetProjectQueryVariables
 } from '@/graphql/generated/graphql'
 import { getClient } from '@/lib/serverClient'
-import { Metadata } from 'next'
+import { Metadata, ResolvingMetadata } from 'next'
 import React from 'react'
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
-export const metadata: Metadata = {
-  title: 'Work | ClickSuite'
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const client = getClient()
+  const data = await client.query<GetProjectQuery, GetProjectQueryVariables>({
+    query: GetProjectDocument,
+    variables: { slug: `/${params.slug}` },
+    context: {
+      fetchOptions: {
+        next: { tags: [params.slug] }
+      }
+    }
+  })
+  return {
+    title: data.data.project?.client + ' | ClickSuite',
+    description: data.data.project?.metaDescription,
+    openGraph: {
+      images: [data.data.project?.openGraphImage?.responsiveImage?.src || '']
+    }
+  }
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
